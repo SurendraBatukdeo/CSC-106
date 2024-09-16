59583fef3769b05e8abf954b21a83e105b28f63e
SurendraBatukdeo <153219478+SurendraBatukdeo@users.noreply.github.com>
2024-09-15 19:48:38 -0500

    Add files via upload

--- Week 03/quiz02.py
+++ Week 03/quiz02.py
@@ -1,1 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Escape Sequence Characters
+
+# # Examples
+
+# In[1]:
+
+
+print("\nHallo")
+
+
+# In[2]:
+
+
+print("\n\nHallo")
+
+
+# In[3]:
+
+
+print("\tHallo")
+
+
+# In[4]:
+
+
+print("                          Hallo")
+
+
+# In[ ]:
+
+
+
+
+
+# # Question
+Using '*' (star) and Escape Sequence Characters create the shape 'M' in University of Mary.  
+End of quiz
+# In[ ]:
+
+
+
 
